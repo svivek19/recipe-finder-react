36138107cc7b906be91e22bcf51225807cff68d9
svivek19 <svivek030503@gmail.com>
2024-02-21 14:16:40 +0530

    feat: separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
+import { Header, AppNameComponent, AppIcon, SearchIcon, SearchComponent, SearchInput, Container } from './component/HeaderComponent';
 
-const Container = styled.div`
-display:flex;
-flex-direction:column;
-`
-const Header = styled.div`
-color:white;
-background-color:darkgreen;
-display:flex;
-justify-content:space-between;
-flex:direction:row;
-padding:20px;
-font-size: 25px;
-font-weight:bold;
-box-shaddow:0 3px 6px #555;
-align-items:center;
-`;
 
-const AppNameComponent = styled.div`
-display:flex;
-align-items:center;
-font-size: 25px;
-`;
-
-const AppIcon = styled.img`
-width:40px;
-height:40px;
-margin-right:10px;
-`;
-
-const SearchIcon = styled.img`
-width:23px;
-height:23px;
-`;
-
-const SearchComponent = styled.div`
-display:flex;
-flex-direction:row;
-background-color: white;
-padding:10px;
-border-radius:6px;
-width:50%;
-`;
-
-const SearchInput = styled.input`
-border:none;
-outline:none;
-width:100%;
-margin-left:15px;
-font-size: 16px;
-font-weight:bold;
-`
 
 const App = () => {
   return (
@@ -63,7 +13,7 @@ const App = () => {
         </AppNameComponent>
         <SearchComponent>
           <SearchIcon src="https://uxwing.com/wp-content/themes/uxwing/download/user-interface/search-icon.png" alt="search-icon" />
-          <SearchInput placeholder='Search Recipe'/>
+          <SearchInput placeholder='Search Recipe' />
         </SearchComponent>
       </Header>
     </Container>
---
+++ src/component/HeaderComponent.jsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+color:white;
+background-color:darkgreen;
+display:flex;
+justify-content:space-between;
+flex:direction:row;
+padding:20px;
+font-size: 25px;
+font-weight:bold;
+box-shaddow:0 3px 6px #555;
+align-items:center;
+`;
+
+export const AppNameComponent = styled.div`
+display:flex;
+align-items:center;
+font-size: 25px;
+`;
+
+export const AppIcon = styled.img`
+width:40px;
+height:40px;
+margin-right:10px;
+`;
+
+export const SearchIcon = styled.img`
+width:23px;
+height:23px;
+`;
+
+export const SearchComponent = styled.div`
+display:flex;
+flex-direction:row;
+background-color: white;
+padding:10px;
+border-radius:6px;
+width:50%;
+`;
+
+export const SearchInput = styled.input`
+border:none;
+outline:none;
+width:100%;
+margin-left:15px;
+font-size: 16px;
+font-weight:bold;
+`;
+
+export const Container = styled.div`
+display:flex;
+flex-direction:column;
+`;
